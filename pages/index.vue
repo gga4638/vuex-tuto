9ae952cdb6195728b2e4c69554808030ab392f34
국경아 <guk4638@naver.com>
2022-04-27 13:43:49 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="text-align: center;">
+    <p>count: {{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters({
+      count: 'count/getCount',
+    }),
+  },
+  methods: {
+    increment() {
+      this.$store.commit('count/increment')
+    },
+    decrement() {
+      this.$store.commit('count/decrement')
+    }
+  }
+}
+</script>
